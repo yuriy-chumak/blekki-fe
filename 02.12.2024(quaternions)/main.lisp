1ef84ba3bddad81e34f9bf5b402d63579740a5ee
Yuriy Chumak <yuriy.chumak@mail.com>
2024-12-14 19:33:11 +0200

    quaternions example

---
+++ 02.12.2024(quaternions)/main.lisp
@@ -0,0 +1,130 @@
+#!/usr/bin/env ol
+; используем зашитые в модель оси координат:
+;  ось Ox - налево, Oy - вверх, Oz - вперед
+
+(import (lib gl-2))
+
+(import (lib soil))
+(import (math infix-notation))
+(import (olite math))
+,load "model.lisp"
+
+(define Carrier-T (read-model-file "Carrier-T"))
+
+; object math:
+; матрица поворота кораблика в пространстве
+; column-major! потому что для OpenGL
+(define rotation (identity-matrix))
+
+; предвычисленные вектора поворота
+(define Lh [1 0 0])   ; вектор "левая рука" (Ox)
+(define Up [0 1 0])   ; вектор "вверх" (Oz)
+(define Fw [0 0 1])   ; вектор "вперед" (Oz)
+; - yaw (рыскание)-------------------------------
+; найдем 1/20 вектора от "левая" до "вперед" (и нормализуем его)
+(define ds (\\ normalize(
+   Lh + (Fw - Lh) / 20
+)))
+
+; найдем кватернион поворота от Lh к Lh
+(define q (rotation-between Lh ds))
+(define yaw (rotation-matrix q))
+
+(define -q (conjugate q))
+(define -yaw (rotation-matrix -q))
+; - roll (крен) ---------------------------------
+(define ds (\\ normalize(
+   Lh + (Up - Lh) / 20
+)))
+(define q (rotation-between Lh ds))
+(define roll (rotation-matrix q))
+(define -q (conjugate q))
+(define -roll (rotation-matrix -q))
+; - pitch (тангаж) ------------------------------
+(define ds (\\ normalize(
+   Up + (Fw - Up) / 20
+)))
+(define q (rotation-between Up ds))
+(define pitch (rotation-matrix q))
+(define -q (conjugate q))
+(define -pitch (rotation-matrix -q))
+; -
+
+; повороты келавиатурой
+(import (lib keyboard))
+(gl:set-keyboard-handler (lambda (key)
+   (case key
+      (KEY_RIGHT
+         (mat4-set! rotation (mat4*mat4 rotation yaw)))
+      (KEY_LEFT
+         (mat4-set! rotation (mat4*mat4 rotation -yaw)))
+      (KEY_UP
+         (mat4-set! rotation (mat4*mat4 rotation pitch)))
+      (KEY_DOWN
+         (mat4-set! rotation (mat4*mat4 rotation -pitch)))
+      (KEY_E
+         (mat4-set! rotation (mat4*mat4 rotation roll)))
+      (KEY_Q
+         (mat4-set! rotation (mat4*mat4 rotation -roll)))
+   )))
+
+; opengl init
+(glEnable GL_DEPTH_TEST)
+
+; opengl draw
+(import (lib GLU))
+(define old (time-ms))
+(gl:set-renderer (lambda ()
+   (glViewport 0 0 (gl:get-window-width) (gl:get-window-height))
+   (glClearColor 0 0 0 1)
+   (glClear (vm:ior GL_COLOR_BUFFER_BIT GL_DEPTH_BUFFER_BIT))
+
+   (define FOVY 45.0)
+   (define ASPECT (/ (gl:get-window-width) (gl:get-window-height)))
+
+   ; --------------------------
+   ; projection matrix
+   (glMatrixMode GL_PROJECTION)
+   (glLoadIdentity)
+   (gluPerspective FOVY ASPECT 0.1 1000)
+
+   ; --------------------------
+   ; modelview matrix
+   (glMatrixMode GL_MODELVIEW)
+   (glLoadIdentity)
+   (gluLookAt 1 2 1.5  ; eye
+              0 0 0  ; center
+              0 1 0) ; up
+
+   ; глобальная система координат
+   (glDisable GL_TEXTURE_2D)
+   (glLineWidth 1)
+   (glBegin GL_LINES)
+      (glColor3f 1 0 0) ; Ox
+      (glVertex3fv [-0.5 0 0])
+      (glVertex3fv [1000 0 0])
+      (glColor3f 0 1 0) ; Oy
+      (glVertex3fv [0 -0.5 0])
+      (glVertex3fv [0 1000 0])
+      (glColor3f 0 0 1) ; Oz
+      (glVertex3fv [0 0 -0.5])
+      (glVertex3fv [0 0 1000])
+   (glEnd)
+   
+   ; draw spaceship
+   (glEnable GL_TEXTURE_2D)
+   (glActiveTexture GL_TEXTURE0)
+
+   ; no more glRotate, just matrix multiplication
+   (glMultMatrixf rotation)
+
+   (glPushMatrix)
+   (glScalef 1/500 1/500 1/500) ; уменьшим оригинальную модельку
+   (draw-textured Carrier-T)
+   (glPopMatrix)
+
+   ; draw model axis for debug
+   (draw-model-axis)
+))
+
+(print "Ok.")
---
+++ 02.12.2024(quaternions)/model.lisp
@@ -0,0 +1,199 @@
+(import
+   (file wavefront obj)
+   (file wavefront mtl))
+(import (owl parse))
+
+
+(define (read-model-file name)
+   ;; LOAD
+
+   (define resources "models/")
+   ; todo: (or fasl-load (string-append "cache/" filename ".fasl")
+
+   ; Load geometry,
+   (define obj-filename (string-append resources name ".obj"))
+   (print "Loading object file " obj-filename "...")
+   (define obj (parse wavefront-obj-parser (file->bytestream obj-filename) obj-filename #t #empty))
+   ; Load materials
+   (define mtl-filename (string-append resources (obj 'mtllib "")))
+   (print "Loading materials file " mtl-filename "...")
+   (define mtl (parse wavefront-mtl-parser (file->bytestream mtl-filename) mtl-filename #t #empty))
+
+   ; (todo: let's convert into inexacts)
+   (define vertices (list->vector (obj 'v #null)))
+   (define normals (list->vector (obj 'vn #null)))
+   (define tcoords (list->vector (obj 'vt #null)))
+
+   ; materials (list of )
+   (define mtls (map (lambda (material) [
+         (material 'name)     ; name
+         (material 'kd)       ; diffuse color
+         (material 'map_kd)]) ; diffuse color texture file
+      mtl))
+
+   ; geometry
+   (define objects (map (lambda (object) [
+         (object 'name)
+         (object 'facegroups)])
+      (obj 'o)))
+   ;; todo: (fasl-save model (string-append "cache/" name ".fasl"))
+
+   ;; COMPILE
+   ; map of name -> [name Kd map_Kd]
+   (define materials (fold (lambda (materials material)
+         (vector-apply material (lambda (name kd map_kd)
+            (define sname (string->symbol name))
+            (put materials sname [
+               sname
+               (vector-map inexact kd)
+               (if map_kd then
+                  (define id (let ((buffer (file->bytevector map_kd)))
+                     (SOIL_load_OGL_texture_from_memory buffer (size buffer) SOIL_LOAD_RGBA SOIL_CREATE_NEW_ID SOIL_FLAG_INVERT_Y))) ; invert y for bmp
+                  ;; (print "map_kd: " map_kd ", id: " id)
+                     ; SOIL_FLAG_MIPMAPS
+                  (glBindTexture GL_TEXTURE_2D id)
+                  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_S GL_REPEAT)
+                  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_T GL_REPEAT)
+                  ;; (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+                  ;; (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
+                  (glBindTexture GL_TEXTURE_2D 0)
+                  id
+               else
+                  0)
+            ]))))
+      {}
+      mtls))
+
+   ;; (define mesh (NewtonMeshCreate newtonian-world))
+   ;; (NewtonMeshBeginBuild mesh)
+
+   (define submodels (reverse
+   (fold (lambda (submodels o)
+         (vector-apply o (lambda (name facegroups)
+            (define index (glGenLists (length facegroups)))
+            (print "compiling object " name "...")
+
+            ;; (define mesh (NewtonMeshCreate newtonian-world))
+            ;; (NewtonMeshBeginBuild mesh)
+
+            (define gllists (reverse
+               (fold (lambda (o group i)
+                        (vector-apply (materials (string->symbol (car group))) (lambda (name kd map_kd)
+                           (glNewList i GL_COMPILE)
+
+                           (glColor4fv kd)
+                           (glBegin GL_TRIANGLES)
+                           (for-each (lambda (face)
+                                 ;; (NewtonMeshBeginFace mesh)
+                                 (for-each (lambda (vertex)
+                                       (vector-apply vertex (lambda (xyz uv n)
+                                          (define vertex (ref vertices xyz))
+                                          (define normal (ref normals n))
+
+                                          (glTexCoord2dv (ref tcoords uv))
+                                          (glNormal3dv normal)
+                                          (glVertex3dv vertex)
+
+                                          ;; (apply NewtonMeshAddPoint (cons mesh (vector->list vertex)))
+                                          ;; (NewtonMeshAddMaterial mesh 0)
+                                          ;; (apply NewtonMeshAddNormal (cons mesh (vector->list normal)))
+                                       )))
+                                       face)
+                                 ;; (NewtonMeshEndFace mesh)
+                                 )
+                              (cdr group))
+                           (glEnd)
+                           (glEndList)
+                           (cons [i map_kd] o)))) ; [list texture]
+                     #null
+                     facegroups
+                     (iota (length facegroups) index))))
+
+            ;; (NewtonMeshEndBuild mesh)
+
+            ;; (define convex (NewtonCreateConvexHullFromMesh newtonian-world mesh 0.0 0))
+            ;; (NewtonCompoundCollisionAddSubCollision collision convex)
+            ;; (NewtonDestroyCollision convex)
+
+            (cons [name gllists] submodels))))
+      #null
+      objects)))
+
+   ;; ;; (NewtonCompoundCollisionEndAddRemove collision)
+
+   ;; (NewtonMeshEndBuild mesh)
+   ;; ;; не работает под андроидом:
+   ;; ;; (define collision (NewtonCreateCompoundCollisionFromMesh newtonian-world mesh 0.1 0 0))
+   ;; (define collision (NewtonCreateNull newtonian-world))
+
+   ;; ;; ; NewtonCollisionCreateInstance + NewtonCollisionGetUserData?
+   ;; ;; (NewtonCompoundCollisionBeginAddRemove collision)
+   ;; ;; (define collision-radius 12) ; todo: брать из свойств корабля
+   ;; ;; (define sphere (NewtonCreateSphere newtonian-world
+   ;; ;;       (* collision-radius PROXIMITY-WARNING-DISTANCE)
+   ;; ;;       PROXIMITY-SPHERE-ID #f)) ; это некая большая сфера,
+   ;; ;;       ; которая должна вызывать "avoid-collision" событие
+   ;; ;; (NewtonCompoundCollisionAddSubCollision collision sphere)
+   ;; ;; (NewtonCompoundCollisionEndAddRemove collision)
+
+   [submodels #|collision|#]) ; todo: add materials? add something else?
+
+
+(define (draw-textured model)
+   (for-each (lambda (submodel)
+         (vector-apply submodel (lambda (name groups)
+            (for-each (lambda (group)
+                  (vector-apply group (lambda (i map_kd)
+                     (glBindTexture GL_TEXTURE_2D map_kd)
+                     (glCallList i))))
+               groups))))
+      (ref model 1)))
+
+(define (draw-geometry model)
+   (for-each (lambda (submodel)
+         (vector-apply submodel (lambda (name groups)
+            (for-each (lambda (group)
+                  (vector-apply group (lambda (i map_kd)
+                     (glCallList i))))
+               groups))))
+      (ref model 1)))
+
+(define (draw-model-axis)
+   ; получим матрицу трансформации модели
+   (define modelview [
+      #i0 #i0 #i0 #i0
+      #i0 #i0 #i0 #i0
+      #i0 #i0 #i0 #i0
+      #i0 #i0 #i0 #i0])
+   (glGetFloatv GL_MODELVIEW_MATRIX modelview)
+   ;; (print modelview)
+
+   ;; (define x0 [0 0 0 1])
+   ;; (define x1 [1 0 0 1])
+   ; повернем оси с помощью матрицы вращения (с демо целью)
+   (define x0 (mat4*vert modelview [0 0 0 1]))
+   (define x1 (mat4*vert modelview [1 0 0 1]))
+   (define y0 (mat4*vert modelview [0 0 0 1]))
+   (define y1 (mat4*vert modelview [0 1 0 1]))
+   (define z0 (mat4*vert modelview [0 0 0 1]))
+   (define z1 (mat4*vert modelview [0 0 1 1]))
+
+   (glPushMatrix)
+   (glLoadIdentity) ; reset matrix to default one
+   (glDisable GL_TEXTURE_2D)
+   (glLineWidth 3)
+   (glBegin GL_LINES)
+      (glColor3f 1 0 0) ; Ox
+      (glVertex3fv x0)
+      (glVertex3fv x1)
+      (glColor3f 0 1 0) ; Oy
+      (glVertex3fv y0)
+      (glVertex3fv y1)
+      (glColor3f 0 0 1) ; Oz
+      (glVertex3fv z0)
+      (glVertex3fv z1)
+   (glEnd)
+
+   ; restore
+   (glPopMatrix)
+   (glColor3f 1 1 1))
